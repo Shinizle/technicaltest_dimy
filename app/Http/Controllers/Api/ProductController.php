97d33061c43600716cb3d7c1e240885885c76c7d
Shinizle <irvankharismafihan@gmail.com>
2022-07-08 04:12:11 +0700

    API - Add New Product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\AddNewProductRequest;
 use App\Http\Requests\Api\GetProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductResourceCollection;
@@ -23,5 +24,21 @@ class ProductController extends Controller
         $data = Product::find($request->id);
 
         return new ProductResource($data);
+    }
+
+    public function addNewProduct(AddNewProductRequest $request)
+    {
+        $data = $this->mapNewProduct($request);
+
+        return new ProductResource($data);
+    }
+
+    private function mapNewProduct($request)
+    {
+        $product = new Product();
+        $product->fill($request->all());
+        $product->save();
+
+        return $product;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'products'], function () {
     Route::get('get-all-products', [ProductController::class, 'getAllProducts']);
     Route::get('get-product', [ProductController::class, 'getProduct']);
+    Route::post('add-new-product', [ProductController::class, 'addNewProduct']);
 });
