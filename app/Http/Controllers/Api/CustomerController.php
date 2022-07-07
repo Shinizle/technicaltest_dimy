01de65736dabee0b4519f594e19556c52423df8f
Shinizle <irvankharismafihan@gmail.com>
2022-07-08 05:46:01 +0700

    API - Create Customer

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\CreateCustomerRequest;
+use App\Http\Resources\CustomerResource;
 use App\Http\Resources\CustomerResourceCollection;
 use App\Models\Customer;
 use Illuminate\Http\Request;
@@ -14,5 +16,21 @@ class CustomerController extends Controller
         $data = Customer::paginate();
 
         return new CustomerResourceCollection($data);
+    }
+
+    public function createCustomer(CreateCustomerRequest $request)
+    {
+        $data = $this->mapNewCustomer($request);
+
+        return new CustomerResource($data);
+    }
+
+    private function mapNewCustomer($request)
+    {
+        $customer = new Customer();
+        $customer->fill($request->all());
+        $customer->save();
+
+        return $customer;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,4 +27,5 @@ Route::group(['prefix' => 'products'], function () {
 
 Route::group(['prefix' => 'customers'], function () {
     Route::get('get-all-customers', [CustomerController::class, 'getAllCustomer']);
+    Route::post('create-customer', [CustomerController::class, 'createCustomer']);
 });
